f469bb4c7b20a2ae5da9ac449654378083e730c2
newtant <the.newtant@gmail.com>
2017-02-21 18:01:49 +0100

    added author name

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ def login():
     print("Logging in...")
     return praw.Reddit('testBot', user_agent = "A test bot that parses new comments. v0.1")
 
-def parse_comments(reddit):
+def parse_comments(reddit, found_comments):
     for comment in reddit.subreddit(sys.argv[2]).comments(limit=25):
-        if sys.argv[1] in comment.body:
-            print("Found " + sys.argv[1] + " in comment with ID " + comment.id + " written by /u/" + comment.id + ".")
+        if sys.argv[1] in comment.body and comment.id not in found_comments:
+            print("Found " + sys.argv[1] + " in comment with ID " + comment.id + " written by /u/" + str(comment.author) + ".")
+            found_comments.append(comment.id)
             #todo: figure out how to get the name of the comment author
             #this doesn't seem to be possible at first glance? what?
             #comment.reply("")
@@ -24,6 +25,7 @@ if __name__ == '__main__':
     if check_args():
         reddit = login()
         print("Logged in as /u/" + str(reddit.user.me()) + ". Starting to parse comments in /r/" + sys.argv[2] + "...")
+        found_comments = []
         # while True:
-        parse_comments(reddit)
+        parse_comments(reddit, found_comments)
         #time.sleep(10)
