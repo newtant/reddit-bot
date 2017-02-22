2e0deca6f0f568d68f659d171f978c2a15daa81a
newtant <the.newtant@gmail.com>
2017-02-22 20:04:28 +0100

    a few more line breaks for readability

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ def parse_comments(reddit, found_comments):
         if "linux" in comment.body.lower() and comment.id not in found_comments:
             print("Found \'linux\' in comment with ID " + comment.id + " written by /u/" + str(comment.author) + ".")
             found_comments.append(comment.id)
+
             comment.reply(get_tuxsay())
             print("Replied successfully!")
+
             with open("comments_replied_to.txt", "a") as f:
                 f.write(comment.id + "\n")
 
@@ -32,22 +34,25 @@ def already_replied_comments():
         with open("comments_replied_to.txt", "r") as f:
             found_comments = f.read()
             found_comments = found_comments.split("\n")
+
     return found_comments
 
 def get_tuxsay():
     sentence = "Regardless of your current choice of OS, you will always be welcome under my wings."
     tux_say = requests.get("https://helloacm.com/api/cowsay/?msg=" + sentence + "&f=tux").json()
     tux_say = tux_say.split("\n")
+
     reply = ""
     for line in tux_say:
         reply += "    " + line + "\n"
+
     return reply
 
 if __name__ == '__main__':
     if check_args():
         reddit = login()
         print("Logged in as /u/" + str(reddit.user.me()) + "! Starting to parse comments in /r/" + sys.argv[1] + "...")
-        
+
         found_comments = already_replied_comments()
 
         while True:
